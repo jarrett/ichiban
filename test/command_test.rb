38eebc829f1b43f6f5de1d1a6178cc592987e905
Jarrett Colby <jarrettcolby@gmail.com>
2013-01-28 01:45:06 -0600

    tests for Ichiban::Command

--- test/command_test.rb
+++ test/command_test.rb
@@ -3,10 +3,19 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'test_helper.rb')
 # No need to do an end-to-end test here. Just mock the classes that Ichiban::Command calls.
 class TestCommand < MiniTest::Unit::TestCase
   def test_new_command
-    skip
+    dest = File.expand_path 'my_site'
+    mock_generator = mock('project generator')
+    Ichiban::ProjectGenerator.expects(:new).with(dest).returns(mock_generator)
+    mock_generator.expects(:generate).with
+    Ichiban::Command.new(['new', 'my_site']).run
   end
   
   def test_watch_command
-    skip
+    cwd = Dir.getwd
+    Ichiban.expects('project_root='.to_sym).with(cwd)
+    mock_watcher = mock('watcher')
+    Ichiban::Watcher.expects(:new).with.returns(mock_watcher)
+    mock_watcher.expects(:start).with
+    Ichiban::Command.new(['watch']).run
   end
 end
